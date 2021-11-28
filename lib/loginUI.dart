da7f489953b0e02f13a440cebbfcf56d6b210401
Krithika230 <72444245+Krithika230@users.noreply.github.com>
2021-11-28 11:46:21 +0530

    Add files via upload

---
+++ lib/loginUI.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/background1.jpg"),
+                fit: BoxFit.cover
+            ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 60.0,
+                    ),
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 70.0,
+                        color: Colors.white,
+                        fontFamily: 'Lobster',
+                        letterSpacing: 5.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter User Name',
+                              hintStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                              hintText: 'User_name'
+                          ),
+                        ),
+                      ),
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter Email ID',
+                              hintStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                              hintText: 'abc@gmail.com'
+                          ),
+                        ),
+                      ),
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                        child: TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter Password',
+                              hintStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                              hintText: 'password'
+                          ),
+                        ),
+                      ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 140,
+                            child: ElevatedButton(
+                              onPressed: (){
+                                // TODO: insert code for login screen
+                              },
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                                shadowColor: MaterialStateProperty.all(Colors.black),
+                              ),
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    color: Colors.blueGrey,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 40,
+                          ),
+                          Container(
+                            height: 50,
+                            width: 140,
+                            child: ElevatedButton(
+                              onPressed: (){
+                                //insert code
+                              },
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                                shadowColor: MaterialStateProperty.all(Colors.black),
+                              ),
+                              child: Text(
+                                'Sign up',
+                                style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                    ),
+                    ElevatedButton(
+                      onPressed: (){
+                        // FORGOT PASSWORD SCREEN GOES HERE
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey),
+                        shadowColor: MaterialStateProperty.all(Colors.blueGrey),
+                      ),
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'loginUI.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
